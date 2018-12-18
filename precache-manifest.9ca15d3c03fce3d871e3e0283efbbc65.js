10339980a5eca3504a69d499f5c6eeabf177e54a
djmorosini <djmorosini@outlook.com>
2018-12-18 18:49:50 -0500

    Updates

---
+++ precache-manifest.9ca15d3c03fce3d871e3e0283efbbc65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da88e857b0c45586b2a3",
+    "url": "/trello-clone/static/js/runtime~main.da88e857.js"
+  },
+  {
+    "revision": "2a57948fa2f4913e839d",
+    "url": "/trello-clone/static/js/main.2a57948f.chunk.js"
+  },
+  {
+    "revision": "1d5948a78a31b4ccf640",
+    "url": "/trello-clone/static/js/1.1d5948a7.chunk.js"
+  },
+  {
+    "revision": "2a57948fa2f4913e839d",
+    "url": "/trello-clone/static/css/main.fceb4b15.chunk.css"
+  },
+  {
+    "revision": "3be4481fe234c80e303ccb7ec8dbe15e",
+    "url": "/trello-clone/index.html"
+  }
+];
